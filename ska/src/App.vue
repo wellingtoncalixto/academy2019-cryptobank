b6c2834933d65ee0a5628ef8b97fbab9ae6457dc
Wellington <wac.monti@outlook.com>
2019-08-01 11:05:12 -0300

    modificando o arquivo App.vue para importar a fonte do projeto e definir um background

--- ska/src/App.vue
+++ ska/src/App.vue
@@ -1,31 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap');
+html{
+  height: 100%;
+  background: #333333;
+  background: url('./assets/fundo@2x.png') no-repeat center fixed;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  height: 100%
 }
 </style>
